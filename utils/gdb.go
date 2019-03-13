edaef715dfe4934531c990b98973c2ca6ca12bf6
milindl <imilindluthra@gmail.com>
2019-03-13 23:29:07 +0530

    Complete Initialization code

---
+++ utils/gdb.go
@@ -0,0 +1,83 @@
+package utils
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"log"
+
+	"github.com/milindl/gdb"
+)
+
+var breakpointHitNotification = make(chan int)
+
+func handleNotifications(notification map[string]interface{}) {
+	if notification["class"] == "library-loaded" || notification["class"] == "library-unloaded" {
+		return
+	}
+	jsonStr, _ := json.Marshal(notification)
+	fmt.Println(string(jsonStr))
+	switch notification["class"] {
+	case "done":
+	case "running":
+		if len(notification) == 1 {
+			<-breakpointHitNotification
+		}
+	case "stopped":
+		breakpointHitNotification <- 1
+	case "error":
+		fmt.Println("TODO: Error handling")
+		fallthrough
+	default:
+		// fmt.Printf("Class = %s\n", notification["class"])
+	}
+}
+
+func SynchronizedSend(gdb *gdb.Gdb, operation string, arguments ...string) (map[string]interface{}) {
+	result, err := gdb.Send(operation, arguments...)
+	CheckError(err)
+	handleNotifications(result)
+	return result
+}
+
+func getSoFilepath() string {
+	dir := os.Getenv("PD_FILE_DIR")
+	if dir == "" {
+		dir_, err := os.Getwd()
+		CheckError(err)
+		dir = dir_
+	}
+
+	fname := fmt.Sprintf("%s/mpic.so", dir)
+	_, err := os.Stat(fname)
+	if err != nil && os.IsNotExist(err) {
+		// Path is wrong for the shared library, throw error.
+		log.Fatalln("Shared object does not exist, set PD_FILE_DIR to directory containing mpic.so")
+	}
+
+	return fname
+}
+
+// InitGDB initializes the GDB interpreter
+func InitGDB(filename string) (rank int, size int) {
+	// start a new instance and pipe the target output to stdout
+	gdb, _ := gdb.New(handleNotifications)
+	go io.Copy(os.Stdout, gdb)
+	// go io.Copy(gdb, os.Stdin)
+	fname := getSoFilepath()
+
+	SynchronizedSend(gdb, "set breakpoint pending on")
+	SynchronizedSend(gdb, "file", filename)
+	SynchronizedSend(gdb, fmt.Sprintf("set exec-wrapper env 'LD_PRELOAD=%s'", fname))
+	SynchronizedSend(gdb, "break PMPI_Init")
+	SynchronizedSend(gdb, "run")
+	SynchronizedSend(gdb, "finish")
+	SynchronizedSend(gdb, "finish")
+
+	// SynchronizedSend(gdb, "c")
+	// gdb.Exit()
+	size = 1
+	rank = 1
+	return
+}
