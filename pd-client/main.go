5ffbdcf8d6fde12cd11ef63650115932d308a3b3
Subhdeep Saha <subhdeepsaha@gmail.com>
2019-03-13 17:40:02 +0530

    Adds GDB initialization code in utils

--- pd-client/main.go
+++ pd-client/main.go
@@ -1,83 +1,9 @@
-// package main
-
-// import (
-// 	"fmt"
-// 	"github.com/cyrus-and/gdb"
-// 	"io"
-// 	"log"
-// 	"os"
-// 	"encoding/json"
-// )
-
-// var breakpointHitNotification = make(chan int)
-
-// func handleNotifications (notification map[string]interface{}) {
-// 	if notification["class"] == "library-loaded" || notification["class"] == "library-unloaded" {
-// 		return
-// 	}
-// 	jsonStr, _ := json.Marshal(notification)
-// 	fmt.Println(string(jsonStr))
-
-// 	if (notification["class"] == "stopped") {
-// 		breakpointHitNotification <- 1
-// 	}
-
-// 	// if (notification["class"] == )
-// }
-
-// func main() {
-// 	// start a new instance and pipe the target output to stdout
-// 	gdb, _ := gdb.New(handleNotifications)
-// 	go io.Copy(os.Stdout, gdb)
-// 	go io.Copy(gdb, os.Stdin)
-
-// 	if len(os.Args) < 2 {
-// 		log.Fatal("Need to have filename to execute")
-// 	}
-
-// 	filename := os.Args[1]
-
-// 	// load and run a program
-// 	result, err := gdb.Send("file-exec-and-symbols", filename)
-
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	handleNotifications(result)
-
-// 	result, err = gdb.Send("break-insert", "4")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	handleNotifications(result)
-
-// 	gdb.Send("exec-run")
-// 	<-breakpointHitNotification
-
-// 	fmt.Println("-------------------- Starting EXEC-NEXT")
-// 	gdb.Send("exec-next")
-// 	fmt.Println("-------------------- Finished EXEC-NEXT")
-// 	<-breakpointHitNotification
-// 	fmt.Println("-------------------- Got 'stop' notification")
-
-// 	fmt.Println("-------------------- Starting DATA-EVAL-EXPRESSION")
-// 	gdb.Send("data-evaluate-expression", "init_debugger()")
-// 	fmt.Println("-------------------- Starting EXEC-RUN")
-// 	gdb.Send("exec-run")
-// 	fmt.Println("-------------------- Finished EXEC-RUN")
-// 	<-breakpointHitNotification
-// 	fmt.Println("-------------------- Got 'stop' notification")
-
-// 	gdb.Exit()
-// }
-
 package main
 
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"net"
 	"os"
 
@@ -85,14 +11,17 @@ import (
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Printf("Usage %s <hostname>:<port>\n", os.Args[0])
+	if len(os.Args) != 3 {
+		fmt.Printf("Usage %s <hostname>:<port> <filename>\n", os.Args[0])
 		os.Exit(1)
 	}
 	conn, err := net.Dial("tcp", os.Args[1])
 	utils.CheckError(err)
 	// client's message
-	fmt.Fprintf(conn, "1,1\n")
+	fmt.Fprintf(conn, "1,1")
+	filename := os.Args[2]
+	utils.InitGDB(filename)
+	log.Printf("GDB Initiaalized\n")
 	for {
 		status, err := bufio.NewReader(conn).ReadString('\n')
 		fmt.Println(status)
--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,8 @@
 package utils
 
-import "log"
+import (
+	"log"
+)
 
 func CheckError(err error) {
 	if err != nil {
